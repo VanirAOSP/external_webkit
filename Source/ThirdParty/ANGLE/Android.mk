76a2617db59a13a273d78714f57432234085a6de
nuclearmistake <nuclearmistake@gmail.com>
2013-07-28 12:59:49 -0400

    Lazy fix for broken aliasing in ANGLE
    Change-Id: I4724c77a200d8233e52e2945ddea30306e1ac6cd

--- Source/ThirdParty/ANGLE/Android.mk
+++ Source/ThirdParty/ANGLE/Android.mk
@@ -25,6 +25,8 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+LOCAL_CFLAGS += -fno-strict-aliasing -Wno-error=strict-aliasing
+
 # Compiler src
 LOCAL_SRC_FILES := \
     BuiltInFunctionEmulator.cpp \
